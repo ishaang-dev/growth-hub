333a89495bf1a4d19135f32ea176f6a64adc353f
Ishaan Gupta <ishaang.dev@gmail.com>
2023-12-27 20:04:05 +0530

    added netlify form

--- script.js
+++ script.js
@@ -1,55 +1,81 @@
-// Smooth scroll animation function
-function scrollToSection(element) {
-  document.querySelector(element).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-}
+console.log(
+  "╔═╗╦═╗╔═╗╦ ╦╔╦╗╦ ╦  ╦ ╦╦ ╦╔╗\n║ ╦╠╦╝║ ║║║║ ║ ╠═╣  ╠═╣║ ║╠╩╗\n╚═╝╩╚═╚═╝╚╩╝ ╩ ╩ ╩  ╩ ╩╚═╝╚═╝"
+);
 
-// Add click event listeners to the anchor links
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll('a[href^="#"]');
-  links.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
+// Set height of each section
+const height = window.innerHeight;
+const sectionElements = document.querySelectorAll("section");
 
-      const targetId = this.getAttribute("href");
-      scrollToSection(targetId);
-    });
-  });
+sectionElements.forEach((section) => {
+  section.style.height = `${height}px`;
 });
 
+// Smooth scroll animation function
+function scrollToSection(element) {
+  document
+    .querySelector(element)
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+function scrollAnimation(event, hyperlink) {
+  event.preventDefault();
+  scrollToSection(hyperlink);
+}
+
+// Handle form submission overriding Netlify
+function submitForm(event) {
+  event.preventDefault();
+
+  const form = document.querySelector('form[name="admissionForm"]');
+  const formData = new FormData(form);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      setTimeout(() => {
+        scrollToSection("#about");
+        setTimeout(() => {
+          alert("Form submitted successfully!");
+        }, 1500);
+      }, 500);
+    })
+    .catch((error) => console.error(error));
+}
+
+// Changes color variables for each section
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section) => {
   if (section.classList.contains("red")) {
-    section.style.setProperty("--tx-color", "var(--tx-red)");
-    section.style.setProperty("--bg-color", "var(--bg-red)");
     section.style.setProperty("--pr-color", "var(--pr-red)");
-    section.style.setProperty("--sc-color", "var(--sc-red)");
     section.style.setProperty("--gr-color", "var(--gr-red)");
-    // Apply other styles as needed
   } else if (section.classList.contains("green")) {
-    section.style.setProperty("--tx-color", "var(--tx-green)");
-    section.style.setProperty("--bg-color", "var(--bg-green)");
     section.style.setProperty("--pr-color", "var(--pr-green)");
-    section.style.setProperty("--sc-color", "var(--sc-green)");
     section.style.setProperty("--gr-color", "var(--gr-green)");
-    // Apply other styles as needed
   } else if (section.classList.contains("purple")) {
-    section.style.setProperty("--tx-color", "var(--tx-purple)");
-    section.style.setProperty("--bg-color", "var(--bg-purple)");
     section.style.setProperty("--pr-color", "var(--pr-purple)");
-    section.style.setProperty("--sc-color", "var(--sc-purple)");
     section.style.setProperty("--gr-color", "var(--gr-purple)");
-    // Apply other styles as needed
   } else if (section.classList.contains("yellow")) {
-    section.style.setProperty("--tx-color", "var(--tx-yellow)");
-    section.style.setProperty("--bg-color", "var(--bg-yellow)");
     section.style.setProperty("--pr-color", "var(--pr-yellow)");
-    section.style.setProperty("--sc-color", "var(--sc-yellow)");
     section.style.setProperty("--gr-color", "var(--gr-yellow)");
-    // Apply other styles as needed
   }
-  // Add more conditions for other classes/colors if needed
+});
+
+// Capitalize input names in form
+document.addEventListener("DOMContentLoaded", function () {
+  const studentNameInput = document.querySelector('input[name="studentName"]');
+  const fatherNameInput = document.querySelector('input[name="fatherName"]');
+
+  if (studentNameInput && fatherNameInput) {
+    studentNameInput.addEventListener("input", function () {
+      this.value = this.value.toUpperCase();
+    });
+
+    fatherNameInput.addEventListener("input", function () {
+      this.value = this.value.toUpperCase();
+    });
+  }
 });
